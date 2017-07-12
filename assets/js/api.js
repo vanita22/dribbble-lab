50a01e2b28e1e732f71973a7c21f371411d4509c
vanita22 <vanitayrafita2004@gmail.com>
2017-07-12 01:00:32 -0300

    actualizando cosigos

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
+
     $(function() {
             $.getJSON('https://api.dribbble.com/v1/users/eduardo/shots?access_token=f3aaee71f13cfc02e29d30301579ee0de33e0646018c0f63d1883d552709e2bb&callback=?', function(resp) {
                 if (resp.data.length > 0) {                         
                     $.each(resp.data.reverse(), function(i, val) {
                         $('#dribbble').prepend(
                         '<li class="box"><img class="cover-card" src="'+val.images.normal+'" /><i class="fa fa-eye" aria-hidden="true"> '+val.views_count+'</i><i class="fa fa-comment-o" aria-hidden="true"> '+val.comments_count+'</i><i class="fa fa-heart" aria-hidden="true"> '+val.likes_count+'</i></li>'
+                        +'<div class="overlay">'+'<div class="text">'+"Hello World"+'</div>'+'</div>'
                         );
                     });
                 }
@@ -13,4 +15,12 @@ $(document).ready(function(){
             }
         });                                     
     });
+
+    
+    /*overlay
+	$(function() {
+		$.getJSON('https://api.dribbble.com/v1/users/eduardo/shots?access_token=f3aaee71f13cfc02e29d30301579ee0de33e0646018c0f63d1883d552709e2bb&callback=?', function(resp) {
+			$('#img-overlay').prepend('<div class="overlay">'+'<div class="text">'+"Hello World"+'</div>'+'</div>');
+		});			  							    
+	});*/
 });
