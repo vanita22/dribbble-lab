2a7263daad769a5d343aab233a2a580b3729d77e
VannPellegrini <vanessapellegrini@hotmail.com>
2017-07-11 23:02:00 -0400

    primer commit, probando overlay

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,17 +1,31 @@
 $(document).ready(function(){
-	$(function() {
-			$.getJSON('https://api.dribbble.com/v1/users/eduardo/shots?access_token=f3aaee71f13cfc02e29d30301579ee0de33e0646018c0f63d1883d552709e2bb&callback=?', function(resp) {
-				if (resp.data.length > 0) {							
-					$.each(resp.data.reverse(), function(i, val) {
-						$('#dribbble').prepend(
-						//'<li class="box"><img src="'+val.images.normal+'" /><h2>'+val.title+'</h2><p><a href="'+val.html_url+'">original post &rarr;</a></p></li>'
-						'<img src="'+val.images.normal+'"/><h2>'+val.title+'</h2><p><a href="'+val.html_url+'">original post &rarr;</a></p>'					
-					);
-				});
-				}
-				else {
-				$('#dribbble').append('<li>No shots.</li>');
-			}
-		});		   							    
-	});
+
+    $(function() {
+
+            $.getJSON('https://api.dribbble.com/v1/users/eduardo/shots?access_token=f3aaee71f13cfc02e29d30301579ee0de33e0646018c0f63d1883d552709e2bb&callback=?', function(resp) {
+
+                if (resp.data.length > 0) {                          
+
+                    $.each(resp.data.reverse(), function(i, val) {
+
+                        $('#dribbble').prepend(
+
+                        '<img src="'+val.images.normal+'"/><h2>'+val.title+'</h2><p><a href="'+val.html_url+'">original post &rarr;</a></p>'                  
+
+                    );
+
+                });
+
+                }
+
+                else {
+
+                $('#dribbble').append('<li>No shots.</li>');
+
+            }
+
+        });                                     
+
+    });
+
 });
