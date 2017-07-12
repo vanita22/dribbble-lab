4cd4e5ad6b6fed57577e0caf33df37f76f56d86a
VannPellegrini <vanessapellegrini@hotmail.com>
2017-07-12 00:07:40 -0400

    overlay aparece pero en el 100% de la pantalla

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,29 @@
 $(document).ready(function(){
 	$(function() {
 			$.getJSON('https://api.dribbble.com/v1/users/eduardo/shots?access_token=f3aaee71f13cfc02e29d30301579ee0de33e0646018c0f63d1883d552709e2bb&callback=?', function(resp) {
+
 				if (resp.data.length > 0) {							
 					$.each(resp.data.reverse(), function(i, val) {
 						$('#dribbble').prepend(
 						//'<li class="box"><img src="'+val.images.normal+'" /><h2>'+val.title+'</h2><p><a href="'+val.html_url+'">original post &rarr;</a></p></li>'
-						'<li class="box"><img src="'+val.images.normal+'" /><h2>'+val.title+'</h2><p><a href="'+val.html_url+'">original post &rarr;</a></p></li>'+
-						'<div class="overlay">'+'<div class="text">'+"Hello World"+'</div>'+'</div>'
+						'<li class="box" id="img-overlay"><img src="'+val.images.normal+'" /><h2>'+val.title+'</h2><p><a href="'+val.html_url+'">original post &rarr;</a></p></li>'
 						);
+						
 					});
 				}
 				else {
 				$('#dribbble').append('<li>No shots.</li>');
 			}
-		});		   							    
+		});	
+		  							    
 	});
+
+		
+	/*overlay
+	$(function() {
+			$.getJSON('https://api.dribbble.com/v1/users/eduardo/shots?access_token=f3aaee71f13cfc02e29d30301579ee0de33e0646018c0f63d1883d552709e2bb&callback=?', function(resp) {
+				$('#img-overlay').prepend('<div class="overlay">'+'<div class="text">'+"Hello World"+'</div>'+'</div>');
+		});	
+		  							    
+	});*/
 });
